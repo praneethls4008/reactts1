74bb135b141a1dbc8bc78c2de081a8b11a4e2730
praneethls4008 <praneethls4008@gmail.com>
2024-01-11 21:03:24 +0530

    retry&loading

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,28 @@
 import { useContext, useEffect, useState } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import './Home.css'
 import { Table } from './Table';
 import { FetchDetailsData } from './FetchDetailsData';
 import { contextProvider } from './App';
+import {DispatcherType } from './App';
+import LoaderComp from "./Loader"
+
+const retryHandler=(searchParam :URLSearchParams, dispatcher :DispatcherType)=>{
+    FetchDetailsData('http://localhost:8000/details', searchParam, dispatcher)
+}
+
 
 export const Home = () => {
     const [searchParam,setSearchParam] = useSearchParams();
     const {tableState, dispatcher} = useContext(contextProvider);
-    FetchDetailsData('http://localhost:8000/details', searchParam, dispatcher);
+    useEffect(()=>FetchDetailsData('http://localhost:8000/details', searchParam, dispatcher)
+    ,[])
+    
     const {data:details, loading:isDetailsLoading, error:isDetailsFetchError} = tableState;
     return(
         <div className="Home">
-            {isDetailsLoading && <div>Loading...</div>}
-            {isDetailsFetchError.error && <div>{isDetailsFetchError.msg}</div>}
+            {isDetailsLoading && <LoaderComp/>}
+            {isDetailsFetchError.error && <button onClick={()=>retryHandler(searchParam, dispatcher)}>Retry</button>}
             {details && <Table searchParam={searchParam}/>}
         </div>
     );
